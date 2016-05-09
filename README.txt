abc6ad36815e20fede2012bd35196856d259144b
Andy Lin <andyl8828@gmail.com>
2016-05-08 23:42:47 -0700

    Tests work for initial install

---
+++ README.txt
@@ -0,0 +1,10 @@
+TEAM 15
+
+Shiqiang Wan A91024714
+Timothy Dea A12415238
+Andy Lin A12211889
+Wing Hung Leung A92420239
+Koji Kameda A10138136
+Ziqing Guo A91069045
+
+Jira link: https://ieng6-apps.ucsd.edu/jira/cs110/secure/RapidBoard.jspa?projectKey=ZZZO&rapidView=1649&view=planning
--- app/src/androidTest/java/tests/locationTest.java
+++ app/src/androidTest/java/tests/locationTest.java
@@ -1,3 +1,7 @@
+/**
+ * Test file for adding, customizing, and deleting locations
+ */
+
 package tests;
 
 import android.os.Handler;
@@ -19,7 +23,6 @@ import coupletones.pro.cse110.coupletones.MapsActivity;
 import coupletones.pro.cse110.coupletones.DataStorage;
 import coupletones.pro.cse110.coupletones.cLocation;
 
-
 /**
  * Created by Andy on 5/7/16.
  */
@@ -28,6 +31,8 @@ public class locationTest extends ActivityInstrumentationTestCase2<MapsActivity>
     MapsActivity mapsActivity;
     GoogleMap gMap;
     String TAG;
+    //flag to wait for thread to finish
+    boolean changedFlag = false;
 
     public locationTest() {
         super(MapsActivity.class);
@@ -36,17 +41,27 @@ public class locationTest extends ActivityInstrumentationTestCase2<MapsActivity>
     @Override
     public void setUp() throws Exception {
         TAG = "setUp";
+        Log.d(TAG, "STARTING setUp!!!!!!!!!!!!!!!!!");
         super.setUp();
         mapsActivity = getActivity();
         gMap = mapsActivity.getMap();
+        changedFlag = false;
     }
 
 
+    /* Test for scenario of user adding a new favorite location without naming it
+     * Given the user is trying to add Geisel Library as a favorite location
+     * When the user taps on and confirms they want to add Geisel Library
+     * Then Geisel Library should be saved as a marker on the map
+     */
     public void test_addDefaultNameFav() {
         TAG = "test_addDefaultNameFav";
         Log.d(TAG, "STARTING test_addDefaultNameFav!!!!!!!!!!!!!!!!!");
+
         final LatLng geiselLatLng= new LatLng(32.881117, -117.237278);
         final cLocation geiselLoc = new cLocation(geiselLatLng, mapsActivity);
+
+        //handler to add marker in google map
         Handler handler = new Handler(Looper.getMainLooper());
         handler.post(new Runnable() {
             @Override
@@ -54,21 +69,35 @@ public class locationTest extends ActivityInstrumentationTestCase2<MapsActivity>
                 final Marker newFav = gMap.addMarker(new MarkerOptions().position(geiselLatLng).
                         title(geiselLoc.getName()));
                 mapsActivity.addMarkerToPref(newFav);
+                //flag to indicate new thread running
+                changedFlag = true;
             }
 
         });
+
+        while (!changedFlag) {
+            //wait for thread to end so sharedPreferences is updated
+        }
 
         assertTrue("Geisel location is favorited", checkName(geiselLoc));
         assertTrue("Geisel location is favorited", checkPosition(geiselLoc));
         Log.d(TAG, "ENDING test_addDefaultNameFav!!!!!!!!!!!!!!!!!");
     }
 
+    /* Test for scenario of user adding a new favorite location and renaming it
+     * Given the user is trying to add UTC Mall as a favorite location with a custom name
+     * When the user taps on and confirms they want to add UTC Mall AND changes the name of
+     * to My Favorite Mall!
+     * Then UTC Mall should be saved as a marker on the map as My Favorite Mall!
+     */
     public void test_addCustomNameFav() {
         TAG = "test_addCustomNameFav";
         Log.d(TAG, "STARTING test_addCustomNameFav!!!!!!!!!!!!!!!!!");
         final LatLng utcLatLng = new LatLng(32.871127, -117.210754);
         final cLocation utcLoc = new cLocation(utcLatLng, mapsActivity);
         final String customName = "My Favorite Mall!";
+
+        //handler to add marker in google map
         Handler handler = new Handler(Looper.getMainLooper());
         handler.post(new Runnable() {
             @Override
@@ -76,21 +105,34 @@ public class locationTest extends ActivityInstrumentationTestCase2<MapsActivity>
                 final Marker newFav = gMap.addMarker(new MarkerOptions().position(utcLatLng).
                         title(customName));
                 mapsActivity.addMarkerToPref(newFav);
+                //flag to indicate new thread running
+                changedFlag = true;
             }
         });
+
+        while (!changedFlag) {
+            //wait for thread to end so sharedPreferences is updated
+        }
+
         utcLoc.setName(customName);
         assertTrue("My Favorite Mall! is favorited", checkName(utcLoc));
         assertTrue("My Favorite Mall! is favorited", checkPosition(utcLoc));
         Log.d(TAG, "ENDING test_addCustomNameFav!!!!!!!!!!!!!!!!!");
     }
 
-
+    /* Test for scenario of user changing a location's name
+     * Given the user has already favorited La Jolla shores and wants to change its name
+     * When the user taps on and changes the name of La Jolla Shores to My Favorite Beach!!!!!!!
+     * Then La Jolla Shores should be updated as a marker on the map with the new name.
+     */
     public void test_changeNameFav() {
         TAG = "test_changeNameFav";
         Log.d(TAG, "STARTING test_changeNameFav!!!!!!!!!!!!!!!!!");
         final LatLng shoresLatLng = new LatLng(32.858167, -117.256469);
         final cLocation shoresLoc = new cLocation(shoresLatLng, mapsActivity);
         final String newName = "My favorite beach!!!!!!";
+
+        //handler to add marker in google map
         Handler handler = new Handler(Looper.getMainLooper());
         handler.post(new Runnable() {
             @Override
@@ -99,8 +141,14 @@ public class locationTest extends ActivityInstrumentationTestCase2<MapsActivity>
                         title(shoresLoc.getName()));
                 mapsActivity.addMarkerToPref(newFav);
                 mapsActivity.changeMarkerName(newFav, newName);
+                //flag to indicate new thread running
+                changedFlag = true;
             }
         });
+
+        while (!changedFlag) {
+            //wait for thread to end so sharedPreferences is updated
+        }
 
         shoresLoc.setName(newName);
         assertTrue("La Jollas Shores location is renamed", checkName(shoresLoc));
@@ -108,11 +156,17 @@ public class locationTest extends ActivityInstrumentationTestCase2<MapsActivity>
         Log.d(TAG, "ENDING test_changeNameFav!!!!!!!!!!!!!!!!!");
     }
 
-
+    /* Test for scenario of user removing a favorite location
+     * Given the user has already favorited Belmont Park and wants to remove it
+     * When the user taps on and chooses to remove Belmont Park as a favorite
+     * Then Belmont Park should not show up / be saved as a favorite.
+     */
     public void test_removeFavorite() {
         TAG = "test_removeFavorite";
         final LatLng belmontParkLatLng = new LatLng(32.770668, -117.251554);
         final cLocation belmontParkLoc = new cLocation(belmontParkLatLng, mapsActivity);
+
+        //handler to add marker to google map
         Handler handler = new Handler(Looper.getMainLooper());
         handler.post(new Runnable() {
             @Override
@@ -121,14 +175,20 @@ public class locationTest extends ActivityInstrumentationTestCase2<MapsActivity>
                         title(belmontParkLoc.getName()));
                 mapsActivity.addMarkerToPref(newFav);
                 mapsActivity.removeMarkerFromMap(newFav);
+                //flag to indicate new thread running
+                changedFlag = true;
             }
         });
+
+        while (!changedFlag) {
+            //wait for thread to end so sharedPreferences is updated
+        }
 
         assertFalse("Belmont Park location is deleted", checkName(belmontParkLoc));
         assertFalse("Belmont Park locatoin is deleted", checkPosition(belmontParkLoc));
     }
 
-
+    /* Private helper method to check if a test location's position is saved in SharedPreferences */
     private boolean checkPosition(cLocation location) {
         TAG = "checkPosition";
         DataStorage dataStorage = mapsActivity.getDS();
@@ -151,6 +211,7 @@ public class locationTest extends ActivityInstrumentationTestCase2<MapsActivity>
         return false;
     }
 
+    /* Private method to check if a test location's name is saved in SharedPrefeerences */
     private boolean checkName(cLocation location) {
         TAG = "checkName";
         DataStorage dataStorage = mapsActivity.getDS();
