2afad8972db121967d5550a7ae0cc990c0350cfd
Tim <t.dea096@gmail.com>
2016-05-22 17:24:59 -0700

    Able to choose notification sound in settings

--- app/src/main/java/coupletones/pro/cse110/coupletones/ShowListActivity.java
+++ app/src/main/java/coupletones/pro/cse110/coupletones/ShowListActivity.java
@@ -1,11 +1,19 @@
 package coupletones.pro.cse110.coupletones;
 
+import android.app.Activity;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.google.android.gms.maps.model.LatLng;
@@ -29,7 +37,8 @@ public class ShowListActivity extends AppCompatActivity
     private ArrayAdapter<String> adapter;
     private ListView listview;
     private int indexOf;
-
+    private String chosenTone;
+    private static final int REQUEST_PICK_TONE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,15 +59,60 @@ public class ShowListActivity extends AppCompatActivity
                     @Override
                     public void onItemClick(AdapterView<?> parent, final View view,
                                             int position, long id) {
+                        indexOf = position;
+                        LayoutInflater inflater = getLayoutInflater();
+                        View dialoglayout = inflater.inflate(R.layout.layout_dialog_partner_location_settings,
+                                null);
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(ShowListActivity.this);
+                        builder.setCancelable(true);
+                        builder.setView(dialoglayout);
+                        builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                        builder.show();
 
                         //Get location clicked by using the position as index in ArrayList
-                        indexOf = position;
-                        DialogFragment dialog = new EditLocationDialog();
-                        dialog.show(getFragmentManager(), getText(R.string.edit_location).toString());
+//                        indexOf = position;
+//                        DialogFragment dialog = new EditLocationDialog();
+//                        dialog.show(getFragmentManager(), getText(R.string.edit_location).toString());
 
                     }
                 });
             }
+    }
+
+    public void showOnMap(View view){
+
+    }
+
+    public void chooseNotification(View view){
+        Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_NOTIFICATION);
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Notification Tone");
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, (Uri) null);
+        startActivityForResult(intent, REQUEST_PICK_TONE);
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent intent)
+    {
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_PICK_TONE)
+        {
+            Uri uri = intent.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
+
+            if (uri != null)
+            {
+                this.chosenTone = uri.toString();
+            }
+            else
+            {
+                Uri defaultTone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                this.chosenTone = defaultTone.toString();
+            }
+        }
     }
 
     /**
