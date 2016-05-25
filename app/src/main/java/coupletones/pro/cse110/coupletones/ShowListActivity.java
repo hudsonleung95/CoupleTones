70f2027c526b9aa37f28c18c57a9cbd8dafc1a49
Tim <t.dea096@gmail.com>
2016-05-24 22:19:23 -0700

    Added list of VibeTones to select from

--- app/src/main/java/coupletones/pro/cse110/coupletones/ShowListActivity.java
+++ app/src/main/java/coupletones/pro/cse110/coupletones/ShowListActivity.java
@@ -2,19 +2,23 @@ package coupletones.pro.cse110.coupletones;
 
 import android.app.Activity;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.Gson;
@@ -39,6 +43,21 @@ public class ShowListActivity extends AppCompatActivity
     private int indexOf;
     private String chosenTone;
     private static final int REQUEST_PICK_TONE = 1;
+    RadioGroup radioGroup;
+
+    //Custom vibration patterns, starts with delay (0) then alternates between vibrate and sleep
+    //in milliseconds
+    long [] vibe1 = {0, 500, 200, 500}; //two long vibrates
+    long [] vibe2 = {0, 250, 100, 250}; //two short vibrates
+    long [] vibe3 = {0, 350, 400, 350}; //two medium vibrates
+    long [] vibe4 = {0, 200, 100, 200, 100, 200}; //three short vibrates
+    long [] vibe5 = {0, 250, 250, 250}; //two short vibrates, medium pause between
+    long [] vibe6 = {0, 250, 200, 500}; //one short vibrate, then long vibrate
+    long [] vibe7 = {0, 500, 200, 250}; //one long vibrate, then short vibrate
+    long [] vibe8 = {0, 250, 100, 250, 100, 500}; //two short vibrates, then long vibrate
+    long [] vibe9 = {0, 500, 200, 250, 200, 250}; //one long vibrate, then two short vibrates
+    long [] vibe10 = {0, 400, 200, 400, 250}; //two medium vibrates, one short vibrate
+    long [][] vibeTones = {vibe1, vibe2, vibe3, vibe4, vibe5, vibe6, vibe7, vibe8, vibe9, vibe10};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,38 +73,76 @@ public class ShowListActivity extends AppCompatActivity
 
         updateList();
 
-            if (listview != null) {
-                listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, final View view,
-                                            int position, long id) {
-                        indexOf = position;
-                        LayoutInflater inflater = getLayoutInflater();
-                        View dialoglayout = inflater.inflate(R.layout.layout_dialog_partner_location_settings,
-                                null);
-                        final AlertDialog.Builder builder = new AlertDialog.Builder(ShowListActivity.this);
-                        builder.setCancelable(true);
-                        builder.setView(dialoglayout);
-                        builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.cancel();
-                            }
-                        });
-                        builder.show();
+        if (listview != null) {
+            listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, final View view,
+                                        int position, long id) {
+                    indexOf = position;
+                    LayoutInflater inflater = getLayoutInflater();
+                    View dialoglayout = inflater.inflate(R.layout.layout_dialog_partner_location_settings,
+                            null);
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(ShowListActivity.this);
+                    builder.setCancelable(true);
+                    builder.setView(dialoglayout);
+                    builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
 
-                        //Get location clicked by using the position as index in ArrayList
+                    builder.show();
+
+
+                    //Get location clicked by using the position as index in ArrayList
 //                        indexOf = position;
 //                        DialogFragment dialog = new EditLocationDialog();
 //                        dialog.show(getFragmentManager(), getText(R.string.edit_location).toString());
 
-                    }
-                });
-            }
+                }
+            });
+        }
     }
 
     public void showOnMap(View view){
+//        Intent showMapIntent = new Intent(this, PartnerMapsActivity.class);
+//        showMapIntent.putExtra("LOC_CLICKED", this.locationNames.get(indexOf));
+//        startActivity(showMapIntent);
+    }
 
+    public void chooseVibeNotification(View view){
+        LayoutInflater inflater = getLayoutInflater();
+        final View vibeToneLayout = inflater.inflate(R.layout.vibrate_notifications_list,
+                null);
+        radioGroup = (RadioGroup) vibeToneLayout.findViewById(R.id.vibe_list_radio_group);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(ShowListActivity.this);
+        builder.setView(vibeToneLayout);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Save Vibe tone
+                int vibePatternIndex = radioGroup.indexOfChild(vibeToneLayout.findViewById(radioGroup.getCheckedRadioButtonId()));
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+              @Override
+              public void onCheckedChanged(RadioGroup group, int checkedId) {
+                  // checkedId is the RadioButton selected
+                  Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                  int vibeIndex = radioGroup.indexOfChild(vibeToneLayout.findViewById(radioGroup.getCheckedRadioButtonId()));
+                  v.vibrate(vibeTones[vibeIndex], -1);
+              }
+        });
+        builder.show();
     }
 
     public void chooseNotification(View view){
@@ -105,12 +162,12 @@ public class ShowListActivity extends AppCompatActivity
 
             if (uri != null)
             {
-                this.chosenTone = uri.toString();
+                chosenTone = uri.toString();
             }
             else
             {
                 Uri defaultTone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-                this.chosenTone = defaultTone.toString();
+                chosenTone = defaultTone.toString();
             }
         }
     }
